71e84339e451a16372eba0f1173dee9daef82a99
Loqhen <80516409+Loqhen@users.noreply.github.com>
2021-03-29 12:59:26 +0200

    Added AdditionalProduction class as one of the SpecialAbility interface

---
+++ src/main/java/it/polimi/ingsw/model/specialability/AdditionalProduction.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model.specialability;
+
+import it.polimi.ingsw.model.enumclasses.AbilityType;
+
+public class AdditionalProduction implements SpecialAbility{
+    @Override
+    public void activate() {
+
+    }
+
+    @Override
+    public AbilityType getType() {
+        return null;
+    }
+}
