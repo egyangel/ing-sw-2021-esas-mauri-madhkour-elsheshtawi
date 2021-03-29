afa0287e18424dd3fd9600f8f6356d706f4a9c69
omeresas <omer.esas@mail.polimi.it>
2021-03-29 19:59:29 +0200

    Resources initilization changed to start at all keys and zero values. DevSlot, PersonalBoard, DefaultProd have been started.

---
+++ src/main/java/it/polimi/ingsw/model/DefaultProd.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.model;
+
+public class DefaultProd {
+    private Resources LHS;
+    private Resources RHS;
+
+    public DefaultProd(){
+        this.LHS = new Resources();
+        this.RHS = new Resources();
+    }
+}
