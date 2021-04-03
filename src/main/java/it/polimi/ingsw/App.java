620da8eb985b27ce8796218612f0542943bdf20f
Amor madhkour <79710817+Amor-Madhkour@users.noreply.github.com>
2021-04-03 22:42:27 +0200

    Update App.java

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -35,7 +35,7 @@ public class App {
 //        playerOne.printStrongBox();
 //        playerOne.useDefProd(ResType.COIN, ResType.SERVANT, ResType.SHIELD);
 //        playerOne.printStrongBox();
-
+        myFunction();
         Resources devCardOneLHS = new Resources(0,0,0,1,0);
         Resources devCardOneRHS = new Resources(0,0,0,0,1);
         Resources devCardOneCost = new Resources(0,2,0,0,0);
@@ -47,6 +47,32 @@ public class App {
         Gson gson = new Gson();
         String devCardString = gson.toJson(devCardtoJson);
         System.out.println(devCardString);
+    }
+    static void myFunction() {
+
+        List<LeaderCard> listOfCards = new ArrayList<>();
+        try (FileReader reader = new FileReader("src/main/java/resouces/LeaderCard.json")) {
+
+
+
+            LeaderCard[] extractedJson = new Gson().fromJson(reader, LeaderCard[].class);
+            //  System.out.println(extractedJson.length);
+            for (int i = 0; i < extractedJson.length; i++) {
+                // System.out.println(extractedJson[i].getAbility().getType());
+                listOfCards.add(new LeaderCard(extractedJson[i].getRequirements(), extractedJson[i].getVictoryPoints(), extractedJson[i].getAbility()));
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (LeaderCard listOfCard : listOfCards) {
+            // System.out.println(extractedJson[i].getAbility().getType());
+
+            System.out.println(listOfCard.getAbility());
+        }
     }
 }
 
