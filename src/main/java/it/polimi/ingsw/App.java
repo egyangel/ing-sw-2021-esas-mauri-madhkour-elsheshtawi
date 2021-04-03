b77ffdc05e22b6bfbc8dbd6539ae1b2ace713049
Amor-Madhkour <79710817+Amor-Madhkour@users.noreply.github.com>
2021-04-03 22:38:07 +0200

    Deserialization of LeaderCards but doesn't work completly

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,24 +1,52 @@
 package it.polimi.ingsw;
 
+        import com.google.gson.Gson;
+        import it.polimi.ingsw.model.DevCard;
+        import it.polimi.ingsw.model.LeaderCard;
+        import it.polimi.ingsw.model.Player;
+        import it.polimi.ingsw.model.Resources;
+        import it.polimi.ingsw.model.enumclasses.DevCardColor;
+        import it.polimi.ingsw.model.enumclasses.ResType;
 
-
-import com.google.gson.GsonBuilder;
-import it.polimi.ingsw.model.LeaderCard;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import com.google.gson.Gson;
-import it.polimi.ingsw.model.specialability.SpecialAbility;
+        import java.io.FileNotFoundException;
+        import java.io.FileReader;
+        import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Scanner;
+// import org.*;
 
 public class App {
-
-
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args ) throws IOException {
+//        MarketTray test = new MarketTray();
+//        System.out.println("Initial MarketTray Position:, rows and columns start at 1");
+//        test.MarketTrayDraw();
+//        System.out.println("After 2nd row selection");
+//        test.selectRow(2);
+//        test.MarketTrayDraw();
+//        test.ResourceDraw();
+//        System.out.println("After 3rd column selection");
+//        test.selectColumn(3);
+//        test.MarketTrayDraw();
+//        test.ResourceDraw();
+//
+//        Player playerOne = new Player();
+//        playerOne.giveInitialStrongboxResources(1, 2, 1, 2, 0);
+//        playerOne.printStrongBox();
+//        playerOne.useDefProd(ResType.COIN, ResType.SERVANT, ResType.SHIELD);
+//        playerOne.printStrongBox();
         myFunction();
+        Resources devCardOneLHS = new Resources(0,0,0,1,0);
+        Resources devCardOneRHS = new Resources(0,0,0,0,1);
+        Resources devCardOneCost = new Resources(0,2,0,0,0);
+        DevCard devcardOne = new DevCard(1, DevCardColor.BLUE, devCardOneLHS, devCardOneRHS, devCardOneCost,1);
+        serializeDevCard(devcardOne);
+    }
+
+    private static void serializeDevCard(DevCard devCardtoJson){
+        Gson gson = new Gson();
+        String devCardString = gson.toJson(devCardtoJson);
+        System.out.println(devCardString);
     }
 
     static void myFunction() {
@@ -48,4 +76,3 @@ public class App {
         }
     }
 }
-
---
+++ src/main/java/it/polimi/ingsw/model/DevDeck.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.enumclasses.DevCardColor;
+
+public class DevDeck {
+    private DevCard[][][] cards;
+    private int cardsInDeck;
+
+    private int devDeckLevel;
+
+
+    public DevDeck(){
+        cards=new DevCard [3][4][4]; /* any ideas on how to bind the color and level
+        attributes to make a deck matrix ordered by level and color?*/
+
+
+    }
+
+    public void reset(){
+        DevCardColor[] colors = DevCardColor.values();
+        cardsInDeck=0;
+
+        for (int i = 0; i< colors.length; i++ ){
+            DevCardColor color=colors[i];
+            //cards[cardsInDeck++] = new DevCard(DevCard.getLevel(),DevCardColor ... ,);
+            for (int j=i; j<4;j++){
+                cards[cardsInDeck] = new DevCard[3][4];
+                cards[cardsInDeck] = new DevCard[3][4];
+            }
+
+
+        }
+    }
+    public boolean isEmpty(){
+        return cardsInDeck==0;
+    }
+}
