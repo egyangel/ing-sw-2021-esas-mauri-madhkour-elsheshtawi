53e248744c127acdbbbbcdaa67dd12da605cfde8
Amor-Madhkour <79710817+Amor-Madhkour@users.noreply.github.com>
2021-04-05 09:37:41 +0200

    Fixed the deserialization of polymorphism class SpecialAbility

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -2,17 +2,12 @@ package it.polimi.ingsw;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import com.sun.jdi.event.VMDisconnectEvent;
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 import it.polimi.ingsw.model.DevCard;
 import it.polimi.ingsw.model.LeaderCard;
-import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Resources;
 import it.polimi.ingsw.model.enumclasses.DevCardColor;
-import it.polimi.ingsw.model.enumclasses.ResType;
 import it.polimi.ingsw.model.specialability.*;
-import org.w3c.dom.events.Event;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -58,47 +53,54 @@ public class App {
         System.out.println(devCardString);
     }
 
-    static void myFunction() {
 
+    static void myFunction2() {
         List<LeaderCard> listOfCards = new ArrayList<>();
+        LeaderCard[] extractedJson = new LeaderCard[0];
+
         try (FileReader reader = new FileReader("src/main/java/it/polimi/ingsw/resources/LeaderCards.json")) {
-
-
-            LeaderCard[] extractedJson = new Gson().fromJson(reader, LeaderCard[].class);
-            //  System.out.println(extractedJson.length);
+            
+                Gson g = (new GsonBuilder()).registerTypeAdapterFactory(
+                        RuntimeTypeAdapterFactory
+                                .of(SpecialAbility.class, "type")
+                                .registerSubtype(Discount.class, "Discount")
+                                .registerSubtype(AdditionalProduction.class, "AdditionalProduction")
+                                .registerSubtype(ConvertWhiteMarble.class, "ConvertWhiteMarble")
+                                .registerSubtype(ExstraSlot.class, "ExstraSlot")
+                ).create();
+                        
+                extractedJson = g.fromJson(reader, LeaderCard[].class);
             for (int i = 0; i < extractedJson.length; i++) {
                 // System.out.println(extractedJson[i].getAbility().getType());
                 listOfCards.add(new LeaderCard(extractedJson[i].getRequirements(), extractedJson[i].getVictoryPoints(), extractedJson[i].getAbility()));
 
             }
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            } catch (FileNotFoundException e) {
+             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (LeaderCard listOfCard : listOfCards) {
-            // System.out.println(extractedJson[i].getAbility().getType());
+        for (int i = 0; i < listOfCards.size(); i++) {
+            System.out.println(listOfCards.get(i).getAbility());
         }
-    }
-
-    static void myFunction2() throws FileNotFoundException {
-
-        Gson g = (new GsonBuilder()).registerTypeAdapterFactory(
-                RuntimeTypeAdapterFactory
-                        .of(SpecialAbility.class, "type")
-                        .registerSubtype(Discount.class, "Discount")
-                        .registerSubtype(AdditionalProduction.class, "AdditionalProduction")
-                        .registerSubtype(ConvertWhiteMarble.class, "ConvertWhiteMarble")
-                        .registerSubtype(ExstraSlot.class, "ExstraSlot")
-        ).create();
-
-        try (FileReader reader = new FileReader("src/main/java/it/polimi/ingsw/resources/LeaderCards.json")) {
-            LeaderCard[] extractedJson = g.fromJson(reader, LeaderCard[].class);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
+        public static TypeAdapterFactory getUserTypeAdapter() {
+            return RuntimeTypeAdapterFactory
+                    .of(SpecialAbility.class, "type")
+                    .registerSubtype(Discount.class, "Discount")
+                    .registerSubtype(AdditionalProduction.class, "AdditionalProduction")
+                    .registerSubtype(ConvertWhiteMarble.class, "ConvertWhiteMarble")
+                    .registerSubtype(ExstraSlot.class, "ExstraSlot");
+        }
+
+        public static Gson getGsonWithTypeAdapters() {
+            GsonBuilder builder = new GsonBuilder();
+            builder.registerTypeAdapterFactory(getUserTypeAdapter());
+            return builder.create();
+        }
+
+    }
+
+
