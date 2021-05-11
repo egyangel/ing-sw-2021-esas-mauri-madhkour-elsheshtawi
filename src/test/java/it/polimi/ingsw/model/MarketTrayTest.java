3613acb57683831ad8acc0d66244061134a80acd
Loqhen <80516409+Loqhen@users.noreply.github.com>
2021-05-11 19:50:24 +0200

    Started MarketTrayTest's selectRow method test

--- src/test/java/it/polimi/ingsw/model/MarketTrayTest.java
+++ src/test/java/it/polimi/ingsw/model/MarketTrayTest.java
@@ -1,13 +1,30 @@
 package it.polimi.ingsw.model;
 
-import org.junit.Test;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MarketTrayTest {
 
     @Test
-    public void selectRow() {
-        int row;
+    @Disabled("Not implemented yet")
+    @DisplayName("selects which row to choose from the market tray")
+    public void selectRow(int row) {
+        /*each number in rowOfMarbles represent a Marble color in a row...
+        * (for example 0=white, 1=blue etc...) */
+        List<Integer> rowOfMarbles=List.of(0,1,2,3);
+        Assertions.assertAll(() -> assertEquals(row, 0)
+        );
+
+
+
+
     }
 
     @Test
