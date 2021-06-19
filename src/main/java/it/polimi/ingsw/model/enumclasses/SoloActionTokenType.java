5ebafca8e1bcfafed83190606f07c4bbcb3bd5ed
egyangel <mohamed.raafat@mail.polimi.it>
2021-06-19 12:37:54 +0200

    - ad solo action type class

--- src/main/java/it/polimi/ingsw/model/enumclasses/SoloActionTokenType.java
+++ src/main/java/it/polimi/ingsw/model/enumclasses/SoloActionTokenType.java
@@ -6,6 +6,6 @@ import it.polimi.ingsw.model.Resources;
 
 
 public enum SoloActionTokenType {
-    DISCARD_DEV_CARD, MOVE_CROSS_TOKEN, MOVE_CROSS_TOKE_SHELF;
+    DISCARD_DEV_CARD, MOVE_CROSS_TOKEN, MOVE_CROSS_TOKEN_SHELF;
 
 }
