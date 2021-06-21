250c45a31aec00c73ba16cd1a15011e711936b14
egyangel <mohamed.raafat@mail.polimi.it>
2021-06-21 15:50:46 +0200

    refactor solo Mode

--- src/main/java/it/polimi/ingsw/model/enumclasses/SoloActionTokenType.java
+++ src/main/java/it/polimi/ingsw/model/enumclasses/SoloActionTokenType.java
@@ -6,6 +6,6 @@ import it.polimi.ingsw.model.Resources;
 
 
 public enum SoloActionTokenType {
-    DISCARD_DEV_CARD, MOVE_CROSS_TOKEN, MOVE_CROSS_TOKEN_SHELF;
+    DISCARD_DEV_CARD, MOVE_CROSS_TOKEN_TWO, MOVE_CROSS_TOKEN_ONE_SHELF;
 
 }
